205e077a22a2866bdc7fec4be785d8bf99143441
5000vw <isaackiplangat1999@gmail.com>
2024-01-09 09:29:28 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,8 +1,31 @@
+# app/app.py
+
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from models import db, Owner , Pet
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
